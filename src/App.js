04b30e671741a59a806d6a7145b73b1949ee3a11
Rida Fatima <16ridabaig@gmail.com>
2024-06-22 13:13:32 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "./output.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    let todoInput = e.target.input;
+    let todoInputValue = e.target.input.value;
+    setTodos([...todos, todoInputValue]);
+    todoInput.value = "";
+    console.log(todos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="m-10 flex flex-col justify-center items-center gap-3 max-h-[100vh] h-[100vh] ">
+        <h1 className="text-center text-4xl font-bold">Todo App</h1>
+        <form onSubmit={handleSubmit} className="w-[400px] mt-8 text-2xl" >
+          <div className="todo_list flex gap-3">
+            <input
+              type="text"
+              placeholder="Add Todo..."
+              name="input"
+              className="bg-gray-200 p-3 rounded-md w-full focus:ring-2 ring-gray-300 border-none outline-none"
+            />
+            <input
+              type="submit"
+              value="Add"
+              className="bg-gray-200 p-3  rounded-md active:ring-2 ring-gray-300 ring-offset-2"
+            />
+          </div>
+        </form>
+        <div className="todosConatiner text-2xl">
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index} className="bg-blue-300 p-3 min-w-[400px] my-3 rounded-md " >{todo}</li>
+            ))}
+          </ul>
+        </div>
+      </main>
+    </>
   );
 }
 
