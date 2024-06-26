e68effddb8979f51dcbfa62a79eca4effabb165c
Rida Fatima <16ridabaig@gmail.com>
2024-06-26 02:06:09 -0800

    added completed todos

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import editImage from "../src/assets/images/edit.svg";
 import deleteImage from "../src/assets/images/delete.svg";
 
 function App() {
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")) || []);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
   const [editIndex, setEditIndex] = useState(null);
   const [editTodos, setEditTodos] = useState("");
   const editWindowRef = useRef(null);
+  const [completedTodos, setCompletedTodos] = useState(
+    JSON.parse(localStorage.getItem("completedTodos")) || []
+  );
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
+
+  useEffect(() => {
+    localStorage.setItem("completedTodos", JSON.stringify(completedTodos));
+  }, [completedTodos]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,13 +30,6 @@ function App() {
     if (todoInputValue) {
       setTodos([...todos, todoInputValue]);
       todoInput.value = "";
-    }
-  };
-
-  const deleteTodo = (index) => {
-    if (window.confirm("Do you want to delete it?")) {
-      const updatedTodos = todos.filter((_, i) => i !== index);
-      setTodos(updatedTodos);
     }
   };
 
@@ -51,6 +53,34 @@ function App() {
     }
   };
 
+  const handleComplete = (index, event) => {
+    const completedTodo = todos[index];
+    if (event.target.checked) {
+      setCompletedTodos([...completedTodos, completedTodo]);
+    } else {
+      const updatedCompletedTodos = completedTodos.filter(
+        (_, i) => i !== index
+      );
+      setCompletedTodos(updatedCompletedTodos);
+    }
+  };
+
+  const deleteTodo = (index) => {
+    if (window.confirm("Do you want to delete it?")) {
+      const updatedTodos = todos.filter((_, i) => i !== index);
+      setTodos(updatedTodos);
+
+
+      if (completedTodos) {
+        const completedTodo = todos[index];
+        const updatedCompletedTodos = completedTodos.filter(
+          (todo) => todo !== completedTodo
+        );
+        setCompletedTodos(updatedCompletedTodos);
+      }
+    }
+  };
+
   const closeEditBox = () => {
     editWindowRef.current.style.display = "none";
     setEditIndex(null);
@@ -60,83 +90,101 @@ function App() {
   return (
     <>
       <main className="mx-10 flex flex-col justify-center items-center gap-3 min-h-[100vh] h-[100%]">
-        <h1 className="text-center text-4xl font-bold">Todo App</h1>
-        <form onSubmit={handleSubmit} className="w-[400px] mt-8 text-2xl">
-          <div className="todo_list flex gap-3">
-            <input
-              type="text"
-              placeholder="Add Todo..."
-              name="input"
-              className="bg-gray-300 p-3 rounded-md w-full focus:ring-2 ring-gray-300 border-none outline-none"
-            />
-            <input
-              type="submit"
-              value="Add"
-              className="bg-gray-300 p-3  rounded-md active:ring-2 ring-gray-300 ring-offset-2"
-            />
-          </div>
-        </form>
-        <div className="todosContainer text-2xl">
-          <ul>
-            {todos.map((todo, index) => (
-              <li
-                key={index}
-                id="todo_item"
-                className="bg-blue-300 p-3 min-w-[400px] my-3 rounded-md flex justify-between"
-              >
-                <span>{todo}</span>
-                <div className="flex">
-                  <img
-                    src={editImage}
-                    alt="Edit"
-                    className="w-7 hover:cursor-pointer"
-                    onClick={() => editTodo(index)}
-                  />
-                  <img
-                    src={deleteImage}
-                    alt="Delete"
-                    className="w-7 hover:cursor-pointer"
-                    onClick={() => deleteTodo(index)}
-                  />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div
-          className="bg-gray-300 w-[500px] h-[45vh] opacity-95 absolute rounded-md flex-col items-center justify-center"
-          id="editWindow"
-          ref={editWindowRef}
-          style={{ display: "none" }}
-        >
-          <h1 className="text-3xl text-center my-3 font-bold">Edit</h1>
+        <section className="bg-sky-800 px-8 py-20 rounded-lg backdrop-blur-lg relative">
+          <h1 className="text-center text-4xl font-bold text-white">
+            Todo App
+          </h1>
           <form
-            onSubmit={handleUpdate}
-            className="flex flex-col items-center justify-center"
+            onSubmit={handleSubmit}
+            className="w-[250px] sm:w-[400px] mt-8 text-2xl"
           >
-            <input
-              type="text"
-              id="editedTodo"
-              onChange={handleEditChange}
-              value={editTodos}
-              className="bg-blue-300 p-3 my-3 rounded-md w-[300px] focus:ring-2 ring-gray-300 border-none outline-none"
-            />
-            <input
-              type="submit"
-              id="updateButton"
-              className="rounded-md bg-blue-500 text-white p-2 px-5 text-lg my-1 focus:ring-2 ring-blue-600 ring-offset-2 hover:bg-blue-600"
-              value="Update"
-            />
+            <div className="todo_list flex gap-3">
+              <input
+                type="text"
+                placeholder="Add Todo..."
+                name="input"
+                className="bg-gray-300 p-3 rounded-md w-full focus:ring-2 ring-gray-300 border-none outline-none"
+              />
+              <input
+                type="submit"
+                value="Add"
+                className="bg-gray-300 p-3  rounded-md active:ring-2 ring-gray-300 ring-offset-2"
+              />
+            </div>
           </form>
-          <span
-            id="close"
-            className="text-2xl absolute top-5 right-5 hover:cursor-pointer"
-            title="Close"
-            onClick={closeEditBox}
+          <div className="todosContainer text-2xl">
+            <ul>
+              {todos.map((todo, index) => (
+                <li
+                  key={index}
+                  id="todo_item"
+                  className={`bg-blue-300 p-3 w-[250px] sm:w-[400px] my-3 rounded-md flex justify-between ${
+                    completedTodos.includes(todos[index]) ? "completed" : ""
+                  } `}
+                >
+                  <input
+                    type="checkbox"
+                    name="checkbox"
+                    id="checkbox"
+                    checked={completedTodos.includes(todos[index])}
+                    onChange={(event) => handleComplete(index, event)}
+                  />
+                  <span>{todo}</span>
+                  <div className="flex">
+                    <img
+                      src={editImage}
+                      alt="Edit"
+                      className="w-7 hover:cursor-pointer"
+                      onClick={() => editTodo(index)}
+                    />
+                    <img
+                      src={deleteImage}
+                      alt="Delete"
+                      className="w-7 hover:cursor-pointer"
+                      onClick={() => deleteTodo(index)}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div
+            className="bg-sky-800 w-[100%] h-[100%] opacity-[96%] shadow-lg shadow-current absolute top-0 left-0 rounded-md flex-col items-center justify-center"
+            id="editWindow"
+            ref={editWindowRef}
+            style={{ display: "none" }}
           >
-            X
-          </span>
-        </div>
+            <h1 className="text-3xl text-center my-3 font-bold text-white">
+              Edit
+            </h1>
+            <form
+              onSubmit={handleUpdate}
+              className="flex flex-col items-center justify-center"
+            >
+              <input
+                type="text"
+                id="editedTodo"
+                onChange={handleEditChange}
+                value={editTodos}
+                className="bg-blue-300 p-3 my-3 rounded-md w-[250px] sm:w-[400px] focus:ring-2 text-xl ring-gray-300 border-none outline-none"
+              />
+              <input
+                type="submit"
+                id="updateButton"
+                className="rounded-md bg-sky-500 text-white p-2 px-5 text-xl my-1 focus:ring-2 ring-sky-600 ring-offset-2 hover:bg-sky-500 shadow-sm shadow-sky-400"
+                value="Update"
+              />
+            </form>
+            <span
+              id="close"
+              className="text-2xl absolute top-5 right-5 hover:cursor-pointer text-white"
+              title="Close"
+              onClick={closeEditBox}
+            >
+              X
+            </span>
+          </div>
+        </section>
       </main>
     </>
   );
